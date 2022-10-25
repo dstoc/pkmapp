ac1d24491f3309016f2c98024037559a7cd8f70b
dstoc <539597+dstoc@users.noreply.github.com>
2022-10-25 22:23:30 +1100

    Fix an issue where new sections would be deleted
    * normalize tree after edits that generate new blocks
    * actually attach new sections to the tree during normalization
    * focus the next editable node after edits that generate new blocks

---
+++ test/specs/edit_test.ts
@@ -0,0 +1,54 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {Main} from '../pages/main';
+import {testState} from '../util/test_state';
+
+describe('main', () => {
+  const state = testState(async () => {
+    const main = await new Main().load();
+    return {
+      main,
+      fs: await main.fileSystem
+    };
+  });
+  function inputOutputTest(input, output) {
+    return async () => {
+      const leading = /(\n +)/.exec(input)[1];
+      input = input.replace(new RegExp(leading, 'g'), '\n');
+      output = output.replace(new RegExp(leading, 'g'), '\n');
+      await state.main.opendirButton.click();
+      await browser.waitUntil(state.main.fileInput.isExisting);
+      await state.main.loadButton.click();
+      await browser.waitUntil($('>>>[contenteditable]').isExisting);
+      const inline = $('>>>[contenteditable]');
+      await inline.click();
+      await browser.keys(input.split(''));
+      await browser.waitUntil(state.main.isClean);
+      expect(await state.fs.getFile('test.md')).toEqual(output);
+    };
+  }
+  it('can generate multiple sections', inputOutputTest(
+    `# 1
+     a
+     # 2
+     b`,
+    `# 1
+     a
+     
+     # 2
+     b
+     `,
+  ));
+});
