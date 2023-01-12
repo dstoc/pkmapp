d5083b4c4f33fed384fb6de772c5465f53bcba2d
dstoc <dstoc@users.noreply.github.com>
2023-01-12 10:42:59 +0000

    deploy: 7ead0878cb95ea9b98782944df9dd07c8608c354

--- deps/lit.js
+++ deps/lit.js
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-export * from 'https://unpkg.com/lit?module';
-export * from 'https://unpkg.com/lit/decorators?module';
-export * from 'https://unpkg.com/lit/directives/repeat?module';
+export * from 'lit';
+export * from 'lit/decorators.js';
+export * from 'lit/directives/repeat.js';
 //# sourceMappingURL=lit.js.map
