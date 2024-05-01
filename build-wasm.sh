daf4796fdcbab85be657529bbe97c2197612bde0
dstoc <539597+dstoc@users.noreply.github.com>
2024-05-02 09:34:29 +1000

    Fetch tree-sitter and tree-sitter-markdown from npm

--- build-wasm.sh
+++ build-wasm.sh
@@ -14,36 +14,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-[ -d node_modules/emscripten-sdk-npm/emsdk ] || npx emsdk-checkout || exit 1
-# From tree-sitter/cli/emscripten-version
-npx emsdk install 3.1.37 || exit 1
-npx emsdk activate 3.1.37 || exit 1
 PKMAPP_ROOT=$PWD
-source node_modules/emscripten-sdk-npm/emsdk/emsdk_env.sh || exit 1
 (
-  unset NODE
   (
     export EXTENSION_TAGS=1
     export EXTENSION_GFM=1
     export NO_DEFAULT_EXTENSIONS=1
     (
-      cd $PKMAPP_ROOT/node_modules/tree-sitter-markdown/tree-sitter-markdown
+      cd $PKMAPP_ROOT/node_modules/@tree-sitter-grammars/tree-sitter-markdown/tree-sitter-markdown
       $PKMAPP_ROOT/node_modules/.bin/tree-sitter generate || exit 1
     )
     (
-      cd $PKMAPP_ROOT/node_modules/tree-sitter-markdown/tree-sitter-markdown-inline
+      cd $PKMAPP_ROOT/node_modules/@tree-sitter-grammars/tree-sitter-markdown/tree-sitter-markdown-inline
       $PKMAPP_ROOT/node_modules/.bin/tree-sitter generate || exit 1
     )
   )
   cd build/deps/
-  $PKMAPP_ROOT/node_modules/.bin/tree-sitter build-wasm $PKMAPP_ROOT/node_modules/tree-sitter-markdown/tree-sitter-markdown || exit 1
-  $PKMAPP_ROOT/node_modules/.bin/tree-sitter build-wasm $PKMAPP_ROOT/node_modules/tree-sitter-markdown/tree-sitter-markdown-inline || exit 1
+  $PKMAPP_ROOT/node_modules/.bin/tree-sitter build --wasm $PKMAPP_ROOT/node_modules/@tree-sitter-grammars/tree-sitter-markdown/tree-sitter-markdown || exit 1
+  $PKMAPP_ROOT/node_modules/.bin/tree-sitter build --wasm $PKMAPP_ROOT/node_modules/@tree-sitter-grammars/tree-sitter-markdown/tree-sitter-markdown-inline || exit 1
 )
-(
-  unset NODE
-  cd third_party/tree-sitter || exit 1
-  script/build-wasm || exit 1
-)
-cp third_party/tree-sitter/lib/binding_web/tree-sitter.js build/deps/
+cp node_modules/web-tree-sitter/tree-sitter.js build/deps/ || exit 1
 echo 'export default TreeSitter;' >> build/deps/tree-sitter.js
-cp third_party/tree-sitter/lib/binding_web/tree-sitter.wasm build/deps/
+cp node_modules/web-tree-sitter/tree-sitter.wasm build/deps/ || exit 1
