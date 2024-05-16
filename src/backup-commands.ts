6998ab619826b3d6a75f0b66548d0881208fa6a5
dstoc <539597+dstoc@users.noreply.github.com>
2024-05-16 13:36:48 +1000

    style: Simplify logic in backup-commands

--- src/backup-commands.ts
+++ src/backup-commands.ts
@@ -3,30 +3,28 @@ import {type Command} from './command-palette.js';
 import {yesNoBundle} from './yes-no-bundle.js';
 
 export function backupCommands(backup: Backup): Command[] {
-  return [
-    ...(backup.hasConfig()
-      ? [
-          {
-            description: 'Turn off backup',
-            execute: async () => {
-              return yesNoBundle({
-                description: 'Are you sure you want to turn off backup?',
-                yes: async () => backup.resetConfiguration(),
-              });
-            },
+  return backup.hasConfig()
+    ? [
+        {
+          description: 'Turn off backup',
+          execute: async () => {
+            return yesNoBundle({
+              description: 'Are you sure you want to turn off backup?',
+              yes: async () => backup.resetConfiguration(),
+            });
           },
-        ]
-      : [
-          {
-            description: 'Turn on backup',
-            execute: async () => {
-              const directory = await showDirectoryPicker({
-                mode: 'readwrite',
-                id: 'backup',
-              });
-              await backup.setConfiguration(directory);
-            },
+        },
+      ]
+    : [
+        {
+          description: 'Turn on backup',
+          execute: async () => {
+            const directory = await showDirectoryPicker({
+              mode: 'readwrite',
+              id: 'backup',
+            });
+            await backup.setConfiguration(directory);
           },
-        ]),
-  ];
+        },
+      ];
 }
