5a8423229e22439dda9120f393e9149fb14156d8
dstoc <539597+dstoc@users.noreply.github.com>
2024-06-18 13:42:21 +1000

    feat: Add a signal util to listen to property changes

--- src/signal-utils.ts
+++ src/signal-utils.ts
@@ -26,3 +26,26 @@ export function sigprop<This extends SigpropHost, Return>(
 
   return result;
 }
+
+export function notify<This, Return>(
+  callback: (target: This, value: Return) => void,
+) {
+  return (
+    target: ClassAccessorDecoratorTarget<This, Return>,
+    _context: ClassAccessorDecoratorContext<This, Return>,
+  ) => {
+    const result: ClassAccessorDecoratorResult<This, Return> = {
+      get(this: This) {
+        // eslint-disable-next-line  @typescript-eslint/no-unsafe-return
+        return target.get.call(this);
+      },
+      set(this: This, value: Return) {
+        if (target.get.call(this) === value) return;
+        target.set.call(this, value);
+        callback(this, value);
+      },
+    };
+
+    return result;
+  };
+}
